a8d0d55f08bf48bf3cc8f1fe36980fdcd3b2cf8b
Hoggardz <116875554+Hoggardz@users.noreply.github.com>
2022-11-08 17:34:34 +0800

    Add a file

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import os
+import pandas
+
+def main():
+    arr = [1,2,3]
+    arr = pandas.array(arr)
+    print("Hello pandas !")
+    print("Hello world ~")
+    return
+
+
+
+if __name__=='__main__':
+    main()
